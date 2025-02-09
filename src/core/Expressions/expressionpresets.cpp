18fcc61ea35c206f6efd663effa8b14a31d07b71
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2025-02-09 02:43:31 +0100

    Expression Presets: add license header

--- src/core/Expressions/expressionpresets.cpp
+++ src/core/Expressions/expressionpresets.cpp
@@ -1,3 +1,25 @@
+/*
+#
+# Friction - https://friction.graphics
+#
+# Copyright (c) Ole-André Rodlie and contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# See 'README.md' for more information.
+#
+*/
+
 #include "expressionpresets.h"
 #include "appsupport.h"
 
