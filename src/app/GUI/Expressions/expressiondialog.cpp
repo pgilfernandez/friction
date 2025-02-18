31e639175161c9735309ece1c4acf04853369fd4
Pablo Gil <pgilfernandez@gmail.com>
2025-02-18 17:17:13 +0100

    when deleting imported preset delete copied one instead of original

--- src/app/GUI/Expressions/expressiondialog.cpp
+++ src/app/GUI/Expressions/expressiondialog.cpp
@@ -926,7 +926,7 @@ void ExpressionDialog::importPreset(const QString& path)
         return;
     }
 
-    const auto expr = mSettings->fExpressions.readExpr(path);
+    auto expr = mSettings->fExpressions.readExpr(path);
     if (mSettings->fExpressions.hasExpr(expr.id)) {
         QMessageBox::warning(this,
                              tr("Expression exists"),
@@ -941,6 +941,7 @@ void ExpressionDialog::importPreset(const QString& path)
                              tr("Save Failed"),
                              tr("Unable to save preset %1.").arg(newPath));
     } else {
+        expr.path = newPath;
         mSettings->fExpressions.addExpr(expr);
         if (!expr.highlighters.isEmpty()) {
             for (const auto &highlight : expr.highlighters) {
