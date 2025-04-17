102fd4fb598f7ce171a7d50ba36597de5731bce6
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2025-04-18 00:00:03 +0200

    Minor

--- src/ui/widgets/aboutwidget.cpp
+++ src/ui/widgets/aboutwidget.cpp
@@ -52,9 +52,6 @@ AboutWidget::AboutWidget(QWidget *parent)
     osName.append(QString(" %1").arg(QApplication::platformName() == "xcb" ? "X11" : QApplication::platformName()));
 #endif
 
-    const QString gpuName = QString("%1 %2").arg(HardwareInfo::sGpuRendererString(),
-                                                 HardwareInfo::sGpuVersionString());
-
     QString label = QString::fromUtf8("<div style=\"margin: 0; padding: 0; text-align: center; font-weight: normal;\">"
                                       "<img src=\":/icons/hicolor/%5x%5/apps/%4.png\" width=\"%2\" height=\"%2\">"
                                       "<h1 style=\"font-weight: normal; margin-top: 0; padding-top: 0;\">%3"
@@ -66,7 +63,7 @@ AboutWidget::AboutWidget(QWidget *parent)
                                                     AppSupport::getAppID(),
                                                     QString::number(ThemeSupport::getIconSize(qRound(logoSize * devicePixelRatioF())).width()),
                                                     osName,
-                                                    gpuName,
+                                                    HardwareInfo::sGpuRendererString(),
                                                     qVersion());
     const auto buildInfo = AppSupport::getAppBuildInfo(true);
     if (!buildInfo.isEmpty()) {
