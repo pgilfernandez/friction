ff148bdf3fb91b92ff1d607cdbf13f97f71d499a
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2025-05-10 23:29:51 +0200

    Added viewer toolbar

---
+++ src/ui/widgets/viewertoolbar.cpp
@@ -0,0 +1,37 @@
+/*
+#
+# Friction - https://friction.graphics
+#
+# Copyright (c) Ole-André Rodlie and contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# See 'README.md' for more information.
+#
+*/
+
+#include "viewertoolbar.h"
+#include "Private/document.h"
+
+using namespace Friction::Ui;
+
+ViewerToolBar::ViewerToolBar(const QString &name,
+                             const QString &title,
+                             QWidget *parent)
+    : ToolBar(name, parent, true)
+{
+    setToolButtonStyle(Qt::ToolButtonIconOnly);
+    setContextMenuPolicy(Qt::NoContextMenu);
+    setAllowedAreas(Qt::TopToolBarArea | Qt::BottomToolBarArea);
+    setWindowTitle(title);
+}
