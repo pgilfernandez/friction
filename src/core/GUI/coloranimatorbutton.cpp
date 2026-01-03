pgilfernandez/friction

commit 6dfd77759e2b8e9c8d1973ea82163f0bfb874a08

beautify ColorAnimatorButton

src/core/GUI/coloranimatorbutton.cpp
@@ -68,10 +68,23 @@ void ColorAnimatorButton::paintEvent(QPaintEvent *) {
     const QColor color = mColorTarget ?
                 mColorTarget->getColor() : mColor;
     QPainter p(this);
-    if(mHover) p.setPen(Qt::red);
-    else p.setPen(Qt::white);
-    p.setBrush(color);
-    p.drawRect(0, 0, width() - 1, height() - 1);
+
+    const QRectF rect(0.0, 0.0, width(), height());
+    const float borderWidth = 2.0;
+    const QRectF innerRect = rect.adjusted(borderWidth, borderWidth, -borderWidth, -borderWidth);
+
+
+    if(mHover) {
+        p.setPen(mColor.darker(130));
+        p.setBrush(mColor.darker(130));
+        p.drawRoundedRect(rect, borderWidth + 2, borderWidth + 2);
+        p.setBrush(mColor);
+        p.drawRoundedRect(innerRect, borderWidth, borderWidth);
+    } else {
+        p.setPen(mColor);
+        p.setBrush(mColor);
+        p.drawRoundedRect(rect, borderWidth + 2, borderWidth + 2);
+    }
 }
 
 void ColorAnimatorButton::openColorSettingsDialog() {

commit 0753cd091d97ff18f062ad7944e7e35cad1d9311

Update coloranimatorbutton.cpp
Fix regression introduced in https://github.com/friction2d/friction/commit/6dfd77759e2b8e9c8d1973ea82163f0bfb874a08

Ref: https://github.com/friction2d/friction/pull/679#issuecomment-3706671982 and https://github.com/friction2d/friction/pull/681

src/core/GUI/coloranimatorbutton.cpp
@@ -71,6 +71,7 @@ void ColorAnimatorButton::setColorTarget(ColorAnimator * const target)
 
 void ColorAnimatorButton::paintEvent(QPaintEvent *)
 {
+    const QColor color = mColorTarget ? mColorTarget->getColor() : mColor;
     QPainter p(this);
 
     const QRectF rect(0.0, 0.0, width(), height());
@@ -81,19 +82,19 @@ void ColorAnimatorButton::paintEvent(QPaintEvent *)
                                            -borderWidth);
 
     if (mHover) {
-        const QColor color = ThemeSupport::getLightDarkColor(mColor, 130);
-        p.setPen(color);
-        p.setBrush(color);
+        const QColor altColor = ThemeSupport::getLightDarkColor(color, 130);
+        p.setPen(altColor);
+        p.setBrush(altColor);
         p.drawRoundedRect(rect,
                           borderWidth + 2,
                           borderWidth + 2);
-        p.setBrush(mColor);
+        p.setBrush(color);
         p.drawRoundedRect(innerRect,
                           borderWidth,
                           borderWidth);
     } else {
-        p.setPen(mColor);
-        p.setBrush(mColor);
+        p.setPen(color);
+        p.setBrush(color);
         p.drawRoundedRect(rect,
                           borderWidth + 2,
                           borderWidth + 2);